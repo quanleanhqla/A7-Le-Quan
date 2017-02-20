5e8f3f8a40fa5a7e0b9839e915510e465d95a86e
Quan Le Anh <quanleanhqla@org.com>
2017-02-21 01:02:37 +0700

    homework 8

---
+++ Pomodoro/HomeWork8/app/src/main/java/com/example/quanla/pomodoro/fragments/TaskDetailFragment.java
@@ -0,0 +1,171 @@
+package com.example.quanla.pomodoro.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.quanla.pomodoro.R;
+import com.example.quanla.pomodoro.activities.MainActivity;
+import com.example.quanla.pomodoro.adapters.ColorAdapter;
+import com.example.quanla.pomodoro.databases.DbContext;
+import com.example.quanla.pomodoro.databases.models.Task;
+import com.example.quanla.pomodoro.decorations.TaskColorDecoration;
+import com.example.quanla.pomodoro.fragments.optionItemStrategies.DoneAddTask;
+import com.example.quanla.pomodoro.fragments.optionItemStrategies.SaveEditTask;
+import com.example.quanla.pomodoro.fragments.optionItemStrategies.Strategy;
+import com.example.quanla.pomodoro.networks.services.AddNewTaskService;
+import com.google.gson.Gson;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskDetailFragment extends Fragment {
+
+    private static final String TAG = "abc";
+
+    @BindView(R.id.edt_taskname)
+    EditText etTaskName;
+
+    @BindView(R.id.edt_payment)
+    EditText etPayment;
+
+    @BindView(R.id.rv_color)
+    RecyclerView rvTaskColor;
+
+    private ColorAdapter taskColorAdapter;
+
+    private String title;
+    private Strategy strategy;
+    private Task task;
+
+    public TaskDetailFragment() {
+        // Required empty public constructor
+        setHasOptionsMenu(true);
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_task_detail, container, false);
+        setupUI(view);
+        return view;
+    }
+
+    private void setupUI(View view) {
+        ButterKnife.bind(this, view);
+
+        taskColorAdapter = new ColorAdapter();
+
+        rvTaskColor.setLayoutManager(new GridLayoutManager(this.getContext(), 4));
+        rvTaskColor.setAdapter(taskColorAdapter);
+        rvTaskColor.addItemDecoration(new TaskColorDecoration());
+
+        if(getActivity() instanceof MainActivity) {
+            ((MainActivity) getActivity()).getSupportActionBar().setTitle(title);
+        }
+
+        if(task !=null){
+
+            etTaskName.setText(task.getName());
+            etPayment.setText(String.valueOf(task.getPayment()));
+            taskColorAdapter.setSelectedColor(task.getColor());
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_edit_task, menu);
+    }
+
+    String taskName;
+    float paymentPerHour;
+    String color;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.m_ok){
+
+            //get data from UI
+            taskName = etTaskName.getText().toString();
+            paymentPerHour = Float.parseFloat(etPayment.getText().toString());
+            color = taskColorAdapter.getSelectedColor();
+
+            if(TaskFragment.viewClick==0){
+                strategy = new SaveEditTask();
+            }
+            else {
+                strategy = new DoneAddTask();
+                addNewTask(taskName, color, paymentPerHour);
+
+            }
+
+            strategy.doOptionItem(task, taskName, paymentPerHour, color);
+
+
+            getActivity().onBackPressed();
+
+
+        }
+        return false;
+    }
+
+    public void addNewTask(String name, String color, float payment){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://a-task.herokuapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        AddNewTaskService addNewTaskService = retrofit.create(AddNewTaskService.class);
+
+        //data & format
+        //format =>Mediatype
+        //date => json
+
+        MediaType jsonType = MediaType.parse("application/json");
+        String addNewTaskJson = (new Gson()).toJson(new Task(name, color, payment));
+        RequestBody addNewBody = RequestBody.create(jsonType, addNewTaskJson);
+
+        addNewTaskService.addTask(addNewBody).enqueue(new Callback<Task>() {
+            @Override
+            public void onResponse(Call<Task> call, Response<Task> response) {
+                Log.d(TAG, String.format("abc: %s", response.body()) );
+            }
+
+            @Override
+            public void onFailure(Call<Task> call, Throwable t) {
+
+            }
+        });
+    }
+
+}
---
+++ Pomodoro/HomeWork8/app/src/main/java/com/example/quanla/pomodoro/networks/services/GetTaskService.java
@@ -0,0 +1,19 @@
+package com.example.quanla.pomodoro.networks.services;
+
+import com.example.quanla.pomodoro.databases.models.Task;
+import com.example.quanla.pomodoro.settings.SharedPrefs;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+/**
+ * Created by QuanLA on 2/20/2017.
+ */
+
+public interface GetTaskService {
+    @GET("task")
+    Call<List<Task>> getAllTask();
+}
